c0373065d5cab1303c328235dc7285ce4f43ec2c
Ray Johnson <rjohnson@yp.com>
2015-03-02 20:05:50 -0800

    export

--- lib/menus.rb
+++ lib/menus.rb
@@ -14,7 +14,7 @@ class MenuBar
 
   def construct_jobs_menu(job_hash)
     win = @main_win
-    @jobs_menu.delete(1, 'end')
+    @jobs_menu.delete(2, 'end')
 
     if job_hash.length > 0
       @jobs_menu.add :separator
@@ -46,12 +46,47 @@ class MenuBar
 
     @jobs_menu = TkMenu.new(menubar)
     @jobs_menu.add :command, :label => 'New Job...', :command => proc{JobUI.new(nil, win)}
+    @jobs_menu.add :command, :label => 'Export...', :command => proc{export_jobs}
 
     menubar.add :cascade, :menu => @jobs_menu, :label => 'Jobs'
   end
 
   def aboutDialog
     puts "About me!"
+  end
+
+  def export_jobs
+    jobs = Job.all
+    jobs.each do |job|
+      puts "#{job.queries.first.name}"
+    end
+    #json = jobs.to_yaml(:Indent => 4, :UseHeader => true, :UseVersion => true)
+    json = jobs.to_json(:except=>[:id, :name], :include=>{:queries=>{:except=>[:id, :job_id]}})
+    puts "hello"
+    puts "JSON: #{json}"
+    data = JSON.parse(json)
+    puts "world"
+    yml = YAML::dump(data)
+    puts "what?"
+    #request = job.to_json(:except=>[:id, :name], :include=>{:queries=>{:except=>[:id, :job_id]}})
+
+    puts "#{yml}"
+    puts "ok"
+ftypes = [
+["Text files", '*txt'],
+["Midi files", '*mid'],
+["Backup files", '*~'],
+["All files", '*']
+]
+
+      fname = Tk.getSaveFile('filetypes' => ftypes,'parent'=> $root,
+        'initialdir' => "#{Dir.home}/Downloads",
+        'initialfile' => "job_export",
+        'defaultextension' => ".yaml"
+      )
+
+      puts "file name: #{fname}"
+
   end
 end
 
